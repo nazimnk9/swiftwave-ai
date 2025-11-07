dde9e18dfce50c22604c8ccf266fb88867a6036d
Md. Nazim Ahmed <77225770+nazimnk9@users.noreply.github.com>
2025-11-07 15:32:18 +0600

    solve issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <ModulesSection/>
       <AIModulesSection/>
       <BrandShow/>
-      <ModulesSection/>
       <TechnicalRequirementsSection/>
       <NextStepsSection />
     </>
